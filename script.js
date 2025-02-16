1f8efb9673c1842adcb1cef4c33a17e810f1afb5
Ivan <109091872+vanaver@users.noreply.github.com>
2025-02-16 20:04:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const logo = document.querySelector('.logo'); // Получаем первый элемент с классом "logo"
+const body = document.body; // `document.body` уже ссылается на <body>, не нужен getElementsByTagName
+const card = document.querySelectorAll('.card') // там 3 карты, 3 элемента
+console.log(card)
+
+
+logo.onclick = () => {
+    let bg = getComputedStyle(body).backgroundImage;
+    if(bg !== 'none') {
+        body.style.background = 'none'
+        body.style.backgroundColor = 'darkcyan';
+        card.forEach((singleCard) => {
+            singleCard.style.backgroundColor = 'blue';
+            singleCard.style.color = 'red';
+        })
+    } else {
+        body.style.background = '';
+        body.style.backgroundAttachment = '';
+        card.forEach((singleCard) => {
+            singleCard.style.backgroundColor = ''; // Возвращаем значение по умолчанию
+            singleCard.style.color = ''; // Возвращаем значение по умолчанию
+        });
+    }
+}
+
